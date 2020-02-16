cb17d8147b9a620fc5a4c17eee4276be0fb55523
Glenn Griffin <ggriffiniii@gmail.com>
2020-02-16 14:58:10 -0800

    improvement

--- src/encode.rs
+++ src/encode.rs
@@ -73,64 +73,58 @@ pub fn hex_to(src: &[u8], dst: &mut [u8]) -> Result<(), Error> {
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 unsafe fn hex_encode_avx2(mut src: &[u8], mut dst: &mut [u8]) {
     while src.len() >= 32 {
-        let tmp = _mm256_loadu_si256(src.as_ptr() as *const _);
-        _mm256_storeu_si256(dst.as_mut_ptr() as *mut _, encode_chunk_avx2(_mm256_castsi256_si128(tmp)));
-        _mm256_storeu_si256(dst.as_mut_ptr().offset(32) as *mut _, encode_chunk_avx2(_mm256_extracti128_si256(tmp, 1)));
+        let input = _mm256_loadu_si256(src.as_ptr() as *const _);
+        _mm256_storeu_si256(dst.as_mut_ptr() as *mut _, encode_chunk_avx2(_mm256_castsi256_si128(input)));
+        _mm256_storeu_si256(dst.as_mut_ptr().offset(32) as *mut _, encode_chunk_avx2(_mm256_extracti128_si256(input, 1)));
         src = &src[32..];
         dst = &mut dst[64..];
     }
-
-    hex_encode_sse41(src, dst);
+    if src.len() >= 16 {
+        let chunk = _mm_loadu_si128(src.as_ptr() as *const _);
+        _mm256_storeu_si256(dst.as_mut_ptr() as *mut _, encode_chunk_avx2(chunk));
+        src = &src[16..];
+        dst = &mut dst[32..];
+    }
+    hex_encode_fallback(src, dst);
 }
 
 #[target_feature(enable="avx2")]
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 unsafe fn encode_chunk_avx2(input: __m128i) -> __m256i {
-    sdbg!(input);
-    let out1 = _mm_shuffle_epi8(input, _mm_setr_epi8(
+    let hi = _mm_shuffle_epi8(input, _mm_setr_epi8(
         0, 1, 2, 3, 4, 5, 6, 7,
         0, 1, 2, 3, 4, 5, 6, 7,
     ));
-    sdbg!(out1);
-    let out2 = _mm_shuffle_epi8(input, _mm_setr_epi8(
+    let lo = _mm_shuffle_epi8(input, _mm_setr_epi8(
         8, 9, 10, 11, 12, 13, 14, 15,
         8, 9, 10, 11, 12, 13, 14, 15,
     ));
-    sdbg!(out2);
-    let out3 = _mm256_set_m128i(out2, out1);
-    mdbg!(out3);
-    let out4 = _mm256_srlv_epi64(out3, _mm256_setr_epi64x(4, 0, 4, 0));
-    mdbg!(out4);
-    let out5 = _mm256_and_si256(out4, _mm256_set1_epi8(0xf));
-    mdbg!(out5);
-    let out6 = _mm256_shuffle_epi8(out5, _mm256_setr_epi8(
+    let joined = _mm256_set_m128i(lo, hi);
+    let shifted = _mm256_srlv_epi64(joined, _mm256_setr_epi64x(4, 0, 4, 0));
+    let masked = _mm256_and_si256(shifted, _mm256_set1_epi8(0xf));
+    let shuffled = _mm256_shuffle_epi8(masked, _mm256_setr_epi8(
         0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15,
         0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15,
     ));
-    mdbg!(out6);
     let offset_lut = _mm256_setr_epi8(
         48, 48, 48, 48, 48, 48, 48, 48, 48, 48,
         87, 87, 87, 87, 87, 87,
         48, 48, 48, 48, 48, 48, 48, 48, 48, 48,
         87, 87, 87, 87, 87, 87,
     );
-    mdbg!(offset_lut);
-    let offsets = _mm256_shuffle_epi8(offset_lut, out6);
-    mdbg!(offsets);
-    let out9 = _mm256_add_epi8(out6, offsets);
-    mdbg!(out9)
+    let offsets = _mm256_shuffle_epi8(offset_lut, shuffled);
+    _mm256_add_epi8(shuffled, offsets)
 }
 
 // copied from https://github.com/Matherunner/bin2hex-sse/blob/master/base16_sse4.cpp
 #[target_feature(enable = "sse4.1")]
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-unsafe fn hex_encode_sse41(mut src: &[u8], dst: &mut [u8]) {
+unsafe fn hex_encode_sse41(mut src: &[u8], mut dst: &mut [u8]) {
     let ascii_zero = _mm_set1_epi8(b'0' as i8);
     let nines = _mm_set1_epi8(9);
     let ascii_a = _mm_set1_epi8((b'a' - 9 - 1) as i8);
     let and4bits = _mm_set1_epi8(0xf);
 
-    let mut i = 0_isize;
     while src.len() >= 16 {
         let invec = _mm_loadu_si128(src.as_ptr() as *const _);
 
@@ -149,14 +143,12 @@ unsafe fn hex_encode_sse41(mut src: &[u8], dst: &mut [u8]) {
         let res1 = _mm_unpacklo_epi8(masked2, masked1);
         let res2 = _mm_unpackhi_epi8(masked2, masked1);
 
-        _mm_storeu_si128(dst.as_mut_ptr().offset(i * 2) as *mut _, res1);
-        _mm_storeu_si128(dst.as_mut_ptr().offset(i * 2 + 16) as *mut _, res2);
+        _mm_storeu_si128(dst.as_mut_ptr() as *mut _, res1);
+        _mm_storeu_si128(dst.as_mut_ptr().offset(16) as *mut _, res2);
         src = &src[16..];
-        i += 16;
+        dst = &mut dst[32..];
     }
-
-    let i = i as usize;
-    hex_encode_fallback(src, &mut dst[i * 2..]);
+    hex_encode_fallback(src, dst);
 }
 
 #[inline]
